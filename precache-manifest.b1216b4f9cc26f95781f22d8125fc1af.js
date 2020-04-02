aa98401fc8431bfd0eb3cf3346b4105e4ee6d65b
Adallah Daknache <abdallah_daknache@hotmail.com>
2020-04-02 18:21:57 +0300

    Updates

---
+++ precache-manifest.b1216b4f9cc26f95781f22d8125fc1af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd8aba0c0536ef2c3f063ff20ac3f5e",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "7b09008e3a2c5542d293",
+    "url": "/hello/static/css/main.265960b5.chunk.css"
+  },
+  {
+    "revision": "3c772d8432b474ae2f51",
+    "url": "/hello/static/js/2.2142576d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.2142576d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b09008e3a2c5542d293",
+    "url": "/hello/static/js/main.981157e3.chunk.js"
+  },
+  {
+    "revision": "81e828f4b151a0a0907a",
+    "url": "/hello/static/js/runtime-main.413ddf28.js"
+  }
+]);
